413b9bc6e3b6dadd70daaa7ab3799328a991f3bd
Will Hart <hart.wl@gmail.com>
2012-08-16 22:51:53 +0100

    Update password setting

--- admin/deploy-secure.sh
+++ admin/deploy-secure.sh
@@ -70,7 +70,7 @@ done
  
 # create a new user for login
 useradd -m -s /bin/bash $userName
-echo $pass1 | passwd $userName --stdin
+echo -e "$pass1\n$pass1\n" | sudo passwd $userName
 
 # login as that user
 sshpass -p $pass1 ssh $userName@localhost
