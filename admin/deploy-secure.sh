ad85de787e8e96b6b2507601e50bfbf385bff384
will-hart <hart.wl@gmail.com>
2012-08-17 13:02:04 +0100

    Update security script password setting

--- admin/deploy-secure.sh
+++ admin/deploy-secure.sh
@@ -66,13 +66,21 @@ useradd -m -s /bin/bash $userName
 #echo -e "$pass1\n$pass1\n" | sudo passwd $userName
 #echo "$userName:$pass1" | chpasswd
 #echo -n '$#@pass1@#$' | passwd $userName --stdin
-passwd $userName
-usermod -a -G admin $userName
 
-if [ $? -gt 0 ]; then
-	echo "Password exited with status - " \ $?
-	exit 1;
-fi
+# loop until a password is set or the user requests abort
+passResult=1
+while [ $passResult -gt 0 ]; do
+	passwd $userName
+	dialog --title "Retry Password" --backtitle "Ubuntu Server Deploy\
+	 Script 1.0" --yesno "Unable to set a new user password.  Do you \
+	 want to try again (select YES) or abort installation (select NO)?" 9 50
+	if [ $? -gt 0 ]; then
+		exit 1; # exit with error
+	fi
+done
+
+# move the user to the admin group
+usermod -a -G admin $userName
 
 # disable root ssh access
 sed -i "s/PermitRootLogin[^ ]/PermitRootLogin no/" /etc/ssh/sshd_config
@@ -81,6 +89,7 @@ sed -i "s/PermitRootLogin[^ ]/PermitRootLogin no/" /etc/ssh/sshd_config
 #sed -i "s/PasswordAuthentication [^ ]/PasswordAuthentication no/" /etc/ssh/sshd_config
 # (COMMENTED OUT AS NEED TO GENERATE ON LOCAL MACHINE FIRST)
 
-# login as that user
+# login as the new user with sudo access
 sshpass -p $pass1 ssh $userName@localhost
+echo "Please enter the password for the new user below:"
 sudo su
