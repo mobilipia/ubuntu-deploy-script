a243f38236d2d5327146cc8280dfbf871aef115d
Will Hart <hart.wl@gmail.com>
2012-08-17 19:56:50 +0100

    Renaming Components to components

---
+++ components/001-apache.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#############################################################
+#	Ubuntu Server Deploy Script (version 1.0)				#
+#															#
+#	001-apache.sh											#
+#		Sets up apache										#
+#															#
+#															#
+#		by William Hart (www.williamhart.info)				#
+#		https://github.com/mecharius/ubuntu-deploy-script	#
+#															#
+#############################################################
+
+apt-get install -y apache2 apache2-prefork-dev libarpl-dev libaprutil1-dev
---
+++ components/002-php.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#############################################################
+#	Ubuntu Server Deploy Script (version 1.0)				#
+#															#
+#	002-php.sh												#
+#		Sets up php											#
+#															#
+#															#
+#		by William Hart (www.williamhart.info)				#
+#		https://github.com/mecharius/ubuntu-deploy-script	#
+#															#
+#############################################################
+
+# install
+apt-get install -y php5 php-pear php-cli php5-mysql php5-suhosin xcache
